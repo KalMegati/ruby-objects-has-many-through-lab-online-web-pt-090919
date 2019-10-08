c810be1071ea69818c780a52a29e1c10ee06d7b6
Srikalyan Megati <kalmegati@gmail.com>
2019-10-08 19:18:31 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,15 @@ class Patient
   end
   
   def new_appointment(doctor, date)
-    Appointment.new()
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select { |checkup| checkup.patient == self }
+  end
+  
+  def doctors
+    self.appointments.map { |checkup| checkup.doctor }
   end
   
 end
